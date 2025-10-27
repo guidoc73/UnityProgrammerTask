d74e4cb5d79fbbe13a61ffbfbbdfcb91438cefaf
Guido Cicciomessere <guidoc_73@hotmail.com>
2025-10-26 21:33:30 -0300

    Added idle, walk and run animations and step sound effects. Refactored Move method

--- Assets/CharacterMovement.cs
+++ Assets/CharacterMovement.cs
@@ -8,6 +8,7 @@ public class CharacterMovement : MonoBehaviour
     [SerializeField] private float _movementSpeed;
     [SerializeField] private float _walkSpeed = 2f;
     [SerializeField] private float _sprintSpeed = 5f;
+    [SerializeField] private float _speedChangeRate = 10.0f;
     private InputAction _moveAction;
     private InputAction _sprintAction;
     
@@ -16,15 +17,22 @@ public class CharacterMovement : MonoBehaviour
     [SerializeField] private float _rotationVelocity = 10;
     private float _targetRotation;
     
+    [InspectorName("SFX")]
+    public AudioClip[] FootstepAudioClips;
+    [Range(0, 1)] public float FootstepAudioVolume = 0.5f;
+    
     private CharacterController _characterController;
     private GameObject _mainCamera;
-
+    private Animator _animator;
+    private float _animationBlend;
+    private int _animIDSpeed;
 
     private void Awake()
     {
         GetMainCamera();
         GetInputActions();
-        SetInitialConfig();
+        InitialAnimationConfig();
+        InitialMovementConfig();
     }
 
     private void Start()
@@ -37,7 +45,13 @@ public class CharacterMovement : MonoBehaviour
         Move();
     }
 
-    private void SetInitialConfig()
+    private void InitialAnimationConfig()
+    {
+        TryGetComponent(out _animator);
+        _animIDSpeed = Animator.StringToHash("Speed");
+    }
+
+    private void InitialMovementConfig()
     {
         _movementSpeed = _walkSpeed;
     }
@@ -56,24 +70,44 @@ public class CharacterMovement : MonoBehaviour
 
     private void Move()
     {
-        var moveValue = _moveAction.ReadValue<Vector2>() * (Time.deltaTime * _movementSpeed);
+        if (_characterController == null || _moveAction == null) 
+            return;
         
-        _characterController.Move(new Vector3(moveValue.x, 0, moveValue.y));
+        var normalizedMovingInputValue = _moveAction.ReadValue<Vector2>().normalized;
+        var direction = normalizedMovingInputValue.normalized;
+        
+        var movement = new Vector3(direction.x, 0, direction.y) * (_movementSpeed * Time.deltaTime);
+        
+        _characterController.Move(movement);
 
-        if (IsMoving(moveValue))
+        var isMoving = IsMoving(movement);
+        
+        if (isMoving)
         {
-            RotateByMovingDirection(moveValue);
+            RotateByMovingDirection(direction);
         }
+        
+        AnimateMovement(isMoving);
     }
 
-    private  bool IsMoving(Vector2 moveValue)
+    private void AnimateMovement(bool isMoving)
     {
-        return moveValue != Vector2.zero;
+        _animationBlend = Mathf.Lerp(_animationBlend, isMoving? _movementSpeed : 0, Time.deltaTime * _speedChangeRate);
+        if (_animationBlend < 0.01f)
+        {
+            _animationBlend = 0f;
+        }
+        _animator.SetFloat(_animIDSpeed, _animationBlend);
     }
 
-    private void RotateByMovingDirection(Vector2 moveValue)
+    private  bool IsMoving(Vector3 moveValue)
     {
-        _targetRotation = Mathf.Atan2(moveValue.x, moveValue.y) * Mathf.Rad2Deg + _mainCamera.transform.eulerAngles.y;
+        return moveValue != Vector3.zero;
+    }
+
+    private void RotateByMovingDirection(Vector2 direction)
+    {
+        _targetRotation = Mathf.Atan2(direction.x, direction.y) * Mathf.Rad2Deg + _mainCamera.transform.eulerAngles.y;
         var rotation = Mathf.SmoothDampAngle(transform.eulerAngles.y, _targetRotation, ref _rotationVelocity,
             _rotationSmoothTime);
 
@@ -96,6 +130,18 @@ public class CharacterMovement : MonoBehaviour
     private void OnSprintCanceled(InputAction.CallbackContext context)
     {
         _movementSpeed = _walkSpeed;
+    }
+    
+    private void OnFootstep(AnimationEvent animationEvent)
+    {
+        if (animationEvent.animatorClipInfo.weight > 0.5f)
+        {
+            if (FootstepAudioClips.Length > 0)
+            {
+                var index = Random.Range(0, FootstepAudioClips.Length);
+                AudioSource.PlayClipAtPoint(FootstepAudioClips[index], transform.TransformPoint(_characterController.center), FootstepAudioVolume);
+            }
+        }
     }
     
     private void OnEnable()
