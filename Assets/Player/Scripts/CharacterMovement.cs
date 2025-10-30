c6a29e8ae6e21f44d5d265814256430f912f2d4a
Guido Cicciomessere <guidoc_73@hotmail.com>
2025-10-30 00:48:59 -0300

    Remove fast run animation, changes on all animations about humanoid and avatar, added aceleration and deceleration and connect with animations.

--- Assets/Player/Scripts/CharacterMovement.cs
+++ Assets/Player/Scripts/CharacterMovement.cs
@@ -7,6 +7,8 @@ namespace Player.Scripts
     [RequireComponent(typeof(CharacterController))]
     public class CharacterMovement : MonoBehaviour
     {
+        private const float IDLE_SPEED = 0;
+
         [Header("Movement Settings")]
         [SerializeField] private float _acceleration;
         [SerializeField] private float _deceleration;
@@ -17,20 +19,21 @@ namespace Player.Scripts
         [SerializeField] private float _rotationSpeed;
         
         private CharacterController _characterController;
+        private Animator _animator;
         
         private InputAction _moveAction;
         private InputAction _sprintAction;
-        private float _targetSpeed;
         private float _resultSpeed;
+        private float _targetSpeed;
+        private float _maxSpeed;
 
         private void Awake()
         {
             _characterController = GetComponent<CharacterController>();
+            _animator = GetComponent<Animator>();
             _moveAction = InputSystem.actions.FindAction("Move");
-            _targetSpeed = _walkSpeed;
             _sprintAction = InputSystem.actions.FindAction("Sprint");
-            _sprintAction.started += OnSprintPressed;
-            _sprintAction.canceled += OnSprintReleased;
+            _targetSpeed = _walkSpeed;
         }
 
         private void Update()
@@ -45,15 +48,22 @@ namespace Player.Scripts
             
             if (moveValue != Vector2.zero)
             {
-                _resultSpeed += _acceleration * Time.deltaTime;
+                _targetSpeed = _sprintAction.IsPressed() ? _sprintSpeed : _walkSpeed;
+
                 transform.forward = GetSmoothRotation(direction); //TODO: hacer que la rotación sea fluida
             }
             else
             {
-                _resultSpeed -= _deceleration * Time.deltaTime;
+                _targetSpeed = IDLE_SPEED;
             }
             
-            _resultSpeed = Mathf.Clamp(_resultSpeed, 0, _targetSpeed);
+            //TODO: Tengo que hacer que el threshold del blend del animator se base en ciertos datos, no hardcodeado
+            //porque si lo cambio en editor no va a quedar prolijo.
+            
+            var rate = _targetSpeed > _resultSpeed ? _acceleration : _deceleration;
+            _resultSpeed = Mathf.MoveTowards(_resultSpeed, _targetSpeed, rate * Time.deltaTime);
+            
+            _animator.SetFloat("Speed", _resultSpeed);
             
             var movement = transform.forward * (_resultSpeed * Time.deltaTime);
 
@@ -66,22 +76,6 @@ namespace Player.Scripts
             
             var resultDirection = Vector3.Lerp(transform.forward, targetDirection, _rotationSpeed * Time.deltaTime);
             return resultDirection;
-        }
-
-        private void OnSprintPressed(InputAction.CallbackContext context)
-        {
-            _targetSpeed = _sprintSpeed;
-        }
-        
-        private void OnSprintReleased(InputAction.CallbackContext context)
-        {
-            _targetSpeed = _walkSpeed;
-        }
-
-        private void OnDestroy()
-        {
-            _sprintAction.started -= OnSprintPressed;
-            _sprintAction.canceled -= OnSprintReleased;
         }
 
         #if UNITY_EDITOR
